a07566afcec3b08ce6079a7b37ea3f703bd851a9
Valentine Gogichashvili <valgog@gmail.com>
2015-03-15 13:04:17 +0100

    some more experiments with pypi

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@
 import os
 from setuptools import setup
 
+__location__ = os.path.join(os.getcwd(), os.path.dirname(inspect.getfile(inspect.currentframe())))
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(__location__, fname)).read()
 
 setup(
     name="pydidit",
-    version="0.1.1",
+    version="0.1.3",
     author="Valentine Gogichashvili",
     author_email="valgog@gmail.com",
     description=("A simple command line script to publish to iDoneThis"),
